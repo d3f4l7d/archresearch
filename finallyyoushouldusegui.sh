20981a1450c37a9ef2fe5072026fe55c461cb1cb
d3f4l7d <idaten.bekko@gmail.com>
2026-04-08 13:34:11 +0900

    Update and rename softinstall.sh to finallyyoushouldusegui.sh

--- finallyyoushouldusegui.sh
+++ finallyyoushouldusegui.sh
@@ -6,8 +6,65 @@ echo " ___) | |_| |  _|   | |  | || |\  |___) || |/ ___ \| |___| |___ "
 echo "|____/ \___/|_|     |_| |___|_| \_|____/ |_/_/   \_\_____|_____|"
 echo
 
+
 sudo pacman -Syu --noconfirm
 
+
+if pacman -Q xfce4 &> /dev/null; then
+    echo "xfce4 is already installed. Skip XFCE4 process..."
+else
+    echo
+    echo "__  _______ ____ _____ _  _   "
+    echo "\ \/ /  ___/ ___| ____| || |  "
+    echo " \  /| |_ | |   |  _| | || |_ "
+    echo " /  \|  _|| |___| |___|__   _|"
+    echo "/_/\_\_|   \____|_____|  |_|  "
+    echo
+
+    echo "select ALL"
+    sudo pacman -S --noconfirm xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
+    sudo systemctl enable lightdm
+    
+    echo "Xfce4 will be applied after reboot"
+    echo "autoinstall of Xfce4 -- DONE"
+fi
+
+
+if pacman -Q gvfs-mtp &> /dev/null; then
+    echo "gvfs is already installed. Skip GVFS process..."
+else
+    echo
+    echo "  ______     _______ ____  "
+    echo " / ___\ \   / /  ___/ ___| "
+    echo "| |  _ \ \ / /| |_  \___ \ "
+    echo "| |_| | \ V / |  _|  ___) |"
+    echo " \____|  \_/  |_|   |____/ "
+    echo
+
+    sudo pacman -S --noconfirm gvfs gvfs-mtp gnome-disk-utility gnome-keyring gvfs-smb samba evince
+
+    echo "autoinstall of mgr -- DONE"
+fi
+
+
+if pacman -Q bluez &> /dev/null; then
+    echo "bluez is already installed. Skip BLUE process..."
+else
+    echo
+    echo "  ____  _    _   _ _____ "
+    echo " | __ )| |  | | | | ____|"
+    echo " |  _ \| |  | | | |  _|  "
+    echo " | |_) | |__| |_| | |___ "
+    echo " |____/|_____\___/|_____|"
+    echo
+
+    sudo pacman -S --noconfirm bluez bluez-utils blueman pipewire-pulse pavucontrol
+    sudo systemctl start bluetooth.service
+    sudo systemctl enable bluetooth.service
+
+    echo "Uncomment #AutoEnable=true in /etc/bluetooth/main.conf"
+    echo "autoinstall of bluetooth -- DONE"
+fi
 
 
 if pacman -Q ibus &> /dev/null; then
@@ -36,106 +93,6 @@ else
     echo "autoinstall of ibus -- DONE"
 fi
 
-if pacman -Q bluez &> /dev/null; then
-    echo "bluez is already installed. Skip BLUE process..."
-else
-    echo
-    echo "  ____  _    _   _ _____ "
-    echo " | __ )| |  | | | | ____|"
-    echo " |  _ \| |  | | | |  _|  "
-    echo " | |_) | |__| |_| | |___ "
-    echo " |____/|_____\___/|_____|"
-    echo
-
-    sudo pacman -S --noconfirm bluez bluez-utils blueman pipewire-pulse pavucontrol
-    sudo systemctl start bluetooth.service
-    sudo systemctl enable bluetooth.service
-
-    echo "Uncomment #AutoEnable=true in /etc/bluetooth/main.conf"
-    echo "autoinstall of bluetooth -- DONE"
-fi
-
-if pacman -Q gvfs-mtp &> /dev/null; then
-    echo "gvfs is already installed. Skip GVFS process..."
-else
-    echo
-    echo "  ______     _______ ____  "
-    echo " / ___\ \   / /  ___/ ___| "
-    echo "| |  _ \ \ / /| |_  \___ \ "
-    echo "| |_| | \ V / |  _|  ___) |"
-    echo " \____|  \_/  |_|   |____/ "
-    echo
-
-    sudo pacman -S --noconfirm gvfs gvfs-mtp gnome-disk-utility gnome-keyring gvfs-smb samba evince
-
-    echo "autoinstall of mgr -- DONE"
-fi
-
-if pacman -Q kicad &> /dev/null; then
-    echo "kicad is already installed. Skip KI process..."
-else
-    echo
-    echo " _  _____ "
-    echo "| |/ /_ _|"
-    echo "| ' / | | "
-    echo "| . \ | | "
-    echo "|_|\_\___|"
-    echo
-
-    sudo pacman -S --noconfirm kicad
-    sudo pacman -S --asdeps --noconfirm kicad-library kicad-library-3d
-    echo "If need Nightly, follow steps below"
-    echo "sudo vim /etc/pacman.conf"
-    echo "Add these:"
-    echo "#Nightly builds for KiCad"
-    echo "[kicad-nightly]"
-    echo "SigLevel=Optional TrustAll"
-    echo "Server=https://kicad.gitlab.io/packaging/kicad-arch/kicad-arch-builder/"
-    echo "sudo pacman -Sy kicad-nightly"
-    echo "autoinstall of KiCad -- DONE"
-fi
-
-if pacman -Q freecad &> /dev/null; then
-    echo "freecad is already installed. Skip CAD process..."
-else
-    echo
-    echo "  ____    _    ____  "
-    echo " / ___|  / \  |  _ \ "
-    echo "| |     / _ \ | | | |"
-    echo "| |___ / ___ \| |_| |"
-    echo " \____/_/   \_\____/ "
-    echo
-
-    sudo pacman -S --noconfirm freecad
-    git clone https://aur.archlinux.org/flashprint.git ~/.config/flashprint
-    cd ~/.config/flashprint
-    makepkg -sirc
-    cd
-
-    echo "autoinstall of Freecad -- DONE"
-fi
-
-if pacman -Q inkscape &> /dev/null; then
-    echo "inkscape is already installed. Skip IS process..."
-else
-    echo
-    echo " ___ ____  "
-    echo "|_ _/ ___| "
-    echo " | |\___ \ "
-    echo " | | ___) |"
-    echo "|___|____/ "
-    echo
-
-    sudo pacman -S --noconfirm inkscape python-tinycss2 typst gtksourceview3 texlive-latex texlive-latexrecommended texlive-latexextra gtk3
-    mkdir ~/.config/textext
-    wget -O ~/.config/textext/TexText-Linux-1.13.0.tar.gz https://github.com/textext/textext/releases/download/1.13.0/TexText-Linux-1.13.0.tar.gz
-    cd ~/.config/textext
-    tar -xvzf TexText-Linux-1.13.0.tar.gz
-    cd ~/.config/textext/textext-1.13.0
-    python3 setup.py
-
-    echo "autoinstall of inkscape -- DONE"
-fi
 
 if pacman -Q libreoffice-fresh &> /dev/null; then
     echo "libreoffice-fresh is already installed. Skip LO process..."
@@ -163,6 +120,7 @@ else
 
     echo "autoinstall of libreoffice -- DONE"
 fi
+
 
 if ! command -v firefox &> /dev/null
 then
@@ -193,6 +151,76 @@ else
     echo "firefox is already installed. Skip FOX process..."
 fi
 
+
+if pacman -Q inkscape &> /dev/null; then
+    echo "inkscape is already installed. Skip IS process..."
+else
+    echo
+    echo " ___ ____  "
+    echo "|_ _/ ___| "
+    echo " | |\___ \ "
+    echo " | | ___) |"
+    echo "|___|____/ "
+    echo
+
+    sudo pacman -S --noconfirm inkscape python-tinycss2 typst gtksourceview3 texlive-latex texlive-latexrecommended texlive-latexextra gtk3
+    mkdir ~/.config/textext
+    wget -O ~/.config/textext/TexText-Linux-1.13.0.tar.gz https://github.com/textext/textext/releases/download/1.13.0/TexText-Linux-1.13.0.tar.gz
+    cd ~/.config/textext
+    tar -xvzf TexText-Linux-1.13.0.tar.gz
+    cd ~/.config/textext/textext-1.13.0
+    python3 setup.py
+
+    echo "autoinstall of inkscape -- DONE"
+fi
+
+
+if pacman -Q freecad &> /dev/null; then
+    echo "freecad is already installed. Skip CAD process..."
+else
+    echo
+    echo "  ____    _    ____  "
+    echo " / ___|  / \  |  _ \ "
+    echo "| |     / _ \ | | | |"
+    echo "| |___ / ___ \| |_| |"
+    echo " \____/_/   \_\____/ "
+    echo
+
+    sudo pacman -S --noconfirm freecad
+    git clone https://aur.archlinux.org/flashprint.git ~/.config/flashprint
+    cd ~/.config/flashprint
+    makepkg -sirc
+    cd
+
+    echo "autoinstall of Freecad -- DONE"
+fi
+
+
+if pacman -Q kicad &> /dev/null; then
+    echo "kicad is already installed. Skip KI process..."
+else
+    echo
+    echo " _  _____ "
+    echo "| |/ /_ _|"
+    echo "| ' / | | "
+    echo "| . \ | | "
+    echo "|_|\_\___|"
+    echo
+
+    sudo pacman -S --noconfirm kicad
+    sudo pacman -S --asdeps --noconfirm kicad-library kicad-library-3d
+    echo "If need Nightly, follow steps below"
+    echo "sudo vim /etc/pacman.conf"
+    echo "Add these:"
+    echo "#Nightly builds for KiCad"
+    echo "[kicad-nightly]"
+    echo "SigLevel=Optional TrustAll"
+    echo "Server=https://kicad.gitlab.io/packaging/kicad-arch/kicad-arch-builder/"
+    echo "sudo pacman -Sy kicad-nightly"
+    echo "autoinstall of KiCad -- DONE"
+fi
+
+
 if pacman -Q vlc &> /dev/null; then
     echo "vlc is already installed. Skip VLC process..."
 else
@@ -210,24 +238,6 @@ else
     echo "autoinstall of vlc -- DONE"
 fi
 
-if pacman -Q xfce4 &> /dev/null; then
-    echo "xfce4 is already installed. Skip XFCE4 process..."
-else
-    echo
-    echo "__  _______ ____ _____ _  _   "
-    echo "\ \/ /  ___/ ___| ____| || |  "
-    echo " \  /| |_ | |   |  _| | || |_ "
-    echo " /  \|  _|| |___| |___|__   _|"
-    echo "/_/\_\_|   \____|_____|  |_|  "
-    echo
-
-    echo "select ALL"
-    sudo pacman -S --noconfirm xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
-    sudo systemctl enable lightdm
-    
-    echo "Xfce4 will be applied after reboot"
-    echo "autoinstall of Xfce4 -- DONE"
-fi
 
 if pacman -Q rapidjson &> /dev/null; then
     echo "rapidjson is already installed. Skip XFCE4 process..."
@@ -253,6 +263,7 @@ else
 
     echo "autoinstall of EPSON scanner drivers -- DONE"
 fi
+
 
 echo
 echo " ____  _____ ____   ___   ___ _____   _   _  _____        __"
